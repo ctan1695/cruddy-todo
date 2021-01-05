84c513565416c23cb6b4fcd2e1a24aba322d49a3
Ernest Tan <ernesttancw@gmail.com>
2021-01-05 12:30:52 +0800

    End of day commit

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,11 +38,11 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  // counter = counter + 1;
+  // return zeroPaddedNumber(counter);
+  readCounter(callback);
 };
-
 
 
 // Configuration -- DO NOT MODIFY //////////////////////////////////////////////
